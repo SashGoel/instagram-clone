d80a878d92cfd201ed1d22bc49f3c1cdb6ef04e7
Sakshi Goel <sakshigoel181@gmail.com>
2023-04-28 14:15:57 +0530

    Update Signup.js

--- client/src/components/screens/Signup.js
+++ client/src/components/screens/Signup.js
@@ -38,7 +38,7 @@ const SignIn = () => {
             M.toast({ html: "invalid email", classes: "#c62828 red darken-3" })
             return
         }
-        fetch("http://localhost:5000/signup", {
+        fetch("https://instagram-clone-34sky6m3g-sashgoel.vercel.app/signup", {
             method: "post",
             headers: {
                 "Content-Type": "application/json"
