206766908f3248b03e8ddf712c265d78a16cf8c3
Sakshi Goel <sakshigoel181@gmail.com>
2023-04-28 15:15:32 +0530

    Update Reset.js

--- client/src/components/screens/Reset.js
+++ client/src/components/screens/Reset.js
@@ -10,7 +10,7 @@ const Reset = () => {
             M.toast({html: "invalid email",classes:"#c62828 red darken-3"})
             return
         }
-        fetch('http://localhost:5000/reset-password',{
+        fetch('https://instagram-clone-34sky6m3g-sashgoel.vercel.app/reset-password',{
             method:"post",
             headers:{
                 "Content-Type":"application/json"
